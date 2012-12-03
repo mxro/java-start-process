fc4827b0d4931fe4914890cc783f97b57a0bdec6
mxro <mxro@nowhere.com>
2012-12-04 11:15:35 +1300

    building implementation

--- src/main/java/de/mxro/process/internal/Engine.java
+++ src/main/java/de/mxro/process/internal/Engine.java
@@ -1,9 +1,11 @@
 package de.mxro.process.internal;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 
 import de.mxro.process.ProcessListener;
 import de.mxro.process.XProcess;
@@ -25,16 +27,60 @@ public class Engine {
 		final InputStream inputStream = process.getInputStream();
 		final InputStream errorStream = process.getErrorStream();
 
+		new StreamReader(inputStream, new StreamListener() {
+
+			@Override
+			public void onOutputLine(final String line) {
+				listener.onOutputLine(line);
+			}
+
+			@Override
+			public void onError(final Throwable t) {
+				listener.onError(new Exception(
+						"Error while reading standard output", t));
+			}
+
+			@Override
+			public void onClosed() {
+				listener.onOutputClosed();
+			}
+		});
+
+		new StreamReader(errorStream, new StreamListener() {
+
+			@Override
+			public void onOutputLine(final String line) {
+				listener.onErrorLine(line);
+			}
+
+			@Override
+			public void onError(final Throwable t) {
+				listener.onError(new Exception(
+						"Error while reading error output", t));
+			}
+
+			@Override
+			public void onClosed() {
+
+			}
+		});
+
 		return new XProcess() {
 
 			@Override
-			public void sendLine(final String line) {
-
+			public synchronized void sendLine(final String line) {
+				final BufferedWriter writer = new BufferedWriter(
+						new OutputStreamWriter(outputStream));
+				try {
+					writer.append(line);
+				} catch (final IOException e) {
+					listener.onError(e);
+				}
 			}
 
 			@Override
 			public void destory() {
-
+				process.destroy();
 			}
 		};
 

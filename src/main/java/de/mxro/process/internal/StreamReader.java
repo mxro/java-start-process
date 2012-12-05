56e3cdf2d02391c74c4b88089cef338bf1ee910c
mxro <mxro@nowhere.com>
2012-12-05 15:44:42 +1300

    fix formatting

--- src/main/java/de/mxro/process/internal/StreamReader.java
+++ src/main/java/de/mxro/process/internal/StreamReader.java
@@ -7,8 +7,6 @@ import java.io.InputStreamReader;
 
 public class StreamReader {
 
-	// private final InputStream stream;
-	// private final StreamListener listener;
 	private final Thread t;
 	private volatile boolean stop = false;
 	private volatile boolean stopped = false;
@@ -26,8 +24,7 @@ public class StreamReader {
 
 	public StreamReader(final InputStream stream, final StreamListener listener) {
 		super();
-		// this.stream = stream;
-		// this.listener = listener;
+
 		this.t = new Thread() {
 
 			@Override
@@ -36,7 +33,7 @@ public class StreamReader {
 						new InputStreamReader(stream));
 				try {
 					String read;
-					System.out.println("start reading");
+
 					while (!reader.ready()) {
 						if (stop) {
 							stream.close();
@@ -46,10 +43,10 @@ public class StreamReader {
 						}
 					}
 					while ((read = reader.readLine()) != null && !stop) {
-						System.out.println("read");
+
 						listener.onOutputLine(read);
 					}
-					System.out.println("done");
+
 					stream.close();
 					stopped = true;
 					listener.onClosed();
